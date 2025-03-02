81330b731d70c841cb4ebcbb8470045e34bf2148
VM_N <necvladm@gmail.com>
2025-03-02 16:40:46 +0200

    update htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,13 +14,20 @@ class HTMLNode():
             result.append(f'{prop}="{self.props[prop]}"')
         return ' '.join(result)
 
-    def __eq__(self, other):
-        return (
-            self.tag == other.tag
-            and self.value == other.value
-            and self.children == other.children
-            and self.props == other.props
-        )
-
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+
+class LeafNode(HTMLNode):
+    def init(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        elif self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
