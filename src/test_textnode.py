af688b53df260c597e72075aade6ac5976d02c22
VM_N <necvladm@gmail.com>
2025-03-04 09:02:15 +0200

    update textnode.py text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -18,6 +18,32 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev/")
         self.assertNotEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_tag_text(self):
+        node = TextNode("Text node", TextType.BOLD)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Text node")
+
+    def test_link(self):
+        node = TextNode("Text node", TextType.LINK, "https://www.boot.dev/")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Text node")
+        self.assertEqual(html_node.props, {'href': "https://www.boot.dev/"})
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://www.boot.dev", "alt": "This is an image"},)
+
     def test_repr(self):
         node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
         self.assertEqual(
