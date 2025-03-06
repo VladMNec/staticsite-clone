96c7793c95ef1ce02928114116dec46934c3e44c
VM_N <necvladm@gmail.com>
2025-03-06 10:53:52 +0200

    add markdown_blocknodes.py and test

---
+++ src/markdown_blocknodes.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_blocktype(block):
+    ordered_start = 1
+    lines = block.split("\n")
+    gen_par = tuple(["#" * i + " " for i in range(1, 7)])
+    if block.startswith(gen_par):
+        return BlockType.HEADING
+    # Check ul block
+    if lines[0].startswith("- "):
+        for line in lines[1:]:
+            if line.startswith("- "):
+                continue
+            return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    # Check quote block
+    if lines[0].startswith("> "):
+        for line in lines[1:]:
+            if line.startswith("> "):
+                continue
+            return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    # Check if it's a complete code block
+    if lines[0] == lines[-1] == "```" and len(lines) > 2:
+        if lines[0] == lines[-1]:
+            return BlockType.CODE
+    # Check if ordered list in incremental order
+    if lines[0].startswith(f"{ordered_start}. "):
+        for line in lines[1:]:
+            ordered_start += 1
+            if line[0:3] == f"{ordered_start}. ":
+                continue
+            return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+def markdown_to_blocks(markdown):
+    if len(markdown) == 0:
+        raise Exception("empty string, nothing to convert")
+    # Split string into markdown blocks and strip empty spaces at the start and end
+    split_block = markdown.split("\n\n")
+    split_block = [block.strip() for block in split_block if len(block.strip())]
+    return split_block
---
+++ src/test_markdown_blocknodes.py
@@ -0,0 +1,43 @@
+import unittest
+from markdown_blocknodes import markdown_to_blocks, block_to_blocktype, BlockType
+
+
+class TestBlock(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_block_to_blocktype(self):
+        block = "# heading"
+        self.assertEqual(block_to_blocktype(block), BlockType.HEADING)
+        block = "## heading2"
+        self.assertEqual(block_to_blocktype(block), BlockType.HEADING)
+        block = "```\ncode\ncode\n```"
+        self.assertEqual(block_to_blocktype(block), BlockType.CODE)
+        block = "> quote\n> another quote"
+        self.assertEqual(block_to_blocktype(block), BlockType.QUOTE)
+        block = "- ul\n- ul2"
+        self.assertEqual(block_to_blocktype(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. list2"
+        self.assertEqual(block_to_blocktype(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_blocktype(block), BlockType.PARAGRAPH)
+
+if __name__ == "__main__":
+    unittest.main()
